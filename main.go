2bd86bd9f2f76c04db7e2e1f5e21384d53be0147
Lucas Lima <lukkasoliveiralima@gmail.com>
2023-02-08 14:03:56 -0300

    create struct doctor

--- main.go
+++ main.go
@@ -2,6 +2,26 @@ package main
 
 import "fmt"
 
+type Doctor struct {
+	ID string
+	Name string
+}
+
+type DoctorList struct {
+	Doctors []Doctor
+}
+
+func (d *DoctorList) Add(doctor Doctor) {
+	d.Doctors = append(d.Doctors, doctor)
+}
+
 func main() {
-	fmt.Println("Hello world.")
+	doctor := Doctor{}
+	doctor.ID = "123456"
+	doctor.Name = "Lucas Lima"
+
+	list := DoctorList{}
+	list.Add(doctor)
+
+	fmt.Println(list)
 }
