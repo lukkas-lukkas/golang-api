655a85a341d8c693bec5fbd6a3f916804a549485
Lucas Lima <lukkasoliveiralima@gmail.com>
2023-03-08 18:16:31 -0300

    create dispatcher to commands

--- main.go
+++ main.go
@@ -4,16 +4,11 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/lukkas-lukkas/go-api-rest/src/domain"
-	console "github.com/lukkas-lukkas/go-api-rest/src/ui/console"
+	"github.com/lukkas-lukkas/go-api-rest/src/ui"
 )
 
 func main() {
 	args := os.Args[1:]
-
-	commands := []domain.Command{
-		console.NewMonitorCommand(),
-	}
 
 	if len(args) < 1 {
 		fmt.Println("You must pass the command")
@@ -22,17 +17,12 @@ func main() {
 
 	subCommand := args[0]
 
-	for _, cmd := range commands {
-		if cmd.Name() == subCommand {
-			cmd.Init(os.Args[2:])
-			err := cmd.Exec()
-			if err != nil {
-				fmt.Println(err)
-			}
-			os.Exit(0)
-		}
+	err := ui.Dispatch(subCommand, args[1:])
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
 
-	fmt.Println("ERROR: Command not found:", subCommand)
-	os.Exit(1)
+	os.Exit(0)
 }
---
+++ src/ui/Dispatcher.go
@@ -0,0 +1,28 @@
+package ui
+
+import (
+	"fmt"
+
+	"github.com/lukkas-lukkas/go-api-rest/src/domain"
+	console "github.com/lukkas-lukkas/go-api-rest/src/ui/console"
+)
+
+func commands() []domain.Command {
+	return []domain.Command{
+		console.NewMonitorCommand(),
+	}
+}
+
+func Dispatch(command string, args []string) error {
+	for _, cmd := range commands() {
+		if cmd.Name() != command {
+			continue
+		}
+
+		cmd.Init(args)
+
+		return cmd.Exec()
+	}
+
+	return fmt.Errorf("ERROR: Command not found: %q", command)
+}
