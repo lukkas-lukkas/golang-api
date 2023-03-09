97f8c18b85cba43cf21bd5e7f0ef90fc0bdf1d12
Lucas Lima <lukkasoliveiralima@gmail.com>
2023-03-09 17:46:19 -0300

    refactor monitoring

--- src/application/MonitorService.go
+++ src/application/MonitorService.go
@@ -1,10 +1,22 @@
 package application
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func Monitor(sites []string, tries int, delay int) {
-	fmt.Println("EXEC MONITOR")
-	fmt.Println("sites", sites)
-	fmt.Println("tries:", tries)
-	fmt.Println("delay:", delay)
+	for i := 1; i <= tries; i++ {
+		fmt.Println("Try number", i)
+		monitorSites(sites, delay)
+	}
+}
+
+func monitorSites(sites []string, delay int) {
+	for _, site := range sites {
+		fmt.Println("Monitoring site:", site)
+	}
+	fmt.Println("")
+
+	time.Sleep(time.Duration(delay) * time.Second)
 }
